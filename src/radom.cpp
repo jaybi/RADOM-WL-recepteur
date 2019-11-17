24268e3d855e54a4309dbf313c68ec1a917cdc2f
jaybi <jbsapis@gmail.com>
2019-11-17 18:43:32 +0100

    Corrections setup virtualwire

--- src/radom.cpp
+++ src/radom.cpp
@@ -119,8 +119,8 @@ void setup() {
 
   // Initialisation de la bibliothèque VirtualWire
   // Vous pouvez changez les broches RX/TX/PTT avant vw_setup() si nécessaire
-  vw_setup(2000);
   vw_set_rx_pin(RX_PIN);
+  vw_setup(2000);
   vw_rx_start(); // On peut maintenant recevoir des messages
 
   if(DEBUG) {// Test de la configuration du numéro de téléphone
